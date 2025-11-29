e5acb0946897e4e038def216d35be1d43ed2a4b9
Amin Israfilov <Amin Israfilov>
2025-11-29 19:50:52 +0000

    4 task

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }

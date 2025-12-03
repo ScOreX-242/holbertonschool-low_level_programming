dfc23936b961767a30cc5e3887720bf5bbe2cc46
Amin Israfilov <israfilovamin@mail.ru>
2025-12-03 08:33:43 +0000

    0 task

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@
 
 /**
  * main - Prints; Positive anything is better than negative nothing
- * Return:Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -16,14 +16,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) {
-	    printf("%d is positive\n", n);
-	}
-	if (n == 0) {
-	    printf("%d is zero\n", n);
-	}
-	if (n < 0) {
-	    printf("%d is negative\n", n);
-	}
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+        
 	return (0);
 }

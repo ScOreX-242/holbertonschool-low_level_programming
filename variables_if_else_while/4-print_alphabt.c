ae3d161e5e4745846d317dd11c52dd9238bfd67c
Amin Israfilov <israfilovamin@mail.ru>
2025-12-04 06:19:43 +0000

    4 task

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,27 @@
 /*
- * File: 2-print_alphabet.c
+ * File: 4-print_alphabt.c
  * Auth: Amin Israfilov <12640@holbertonstudents.com>
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase.
+ * main - Prints the alphabet in lowercase, skipping 'e' and 'q'.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char letter;
-    for (letter = 'a';letter <= 'z'; letter++)
-    if (letter != 'q' && letter != 'e') 
-    {
-        putchar(letter);
-    }
-    putchar('\n');
+	char letter;
 
-    return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }

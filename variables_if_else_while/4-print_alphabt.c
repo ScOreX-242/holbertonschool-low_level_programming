de57fa89a6e6eadf8a3b7ddf19ee42f770a07371
Amin Israfilov <israfilovamin@mail.ru>
2025-12-04 06:18:04 +0000

    4 task

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Amin Israfilov <12640@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char letter;
+    for (letter = 'a';letter <= 'z'; letter++)
+    if (letter != 'q' && letter != 'e') 
+    {
+        putchar(letter);
+    }
+    putchar('\n');
+
+    return (0);
+}

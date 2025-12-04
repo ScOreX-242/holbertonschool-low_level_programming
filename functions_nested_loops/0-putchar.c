9498bd73dc74fc2d1ff9973f78e62a3c03e6e67a
Amin Israfilov <israfilovamin@mail.ru>
2025-12-04 08:14:57 +0000

    0 task

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Amin Israfilov <12640@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Program that prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("_putchar");
+
+	return (0);
+}

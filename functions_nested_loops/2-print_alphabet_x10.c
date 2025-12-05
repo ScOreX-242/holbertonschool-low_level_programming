d91b535b48e054719e4fc0d0f96134120c750ffb
ScOreX-242 <israfilovamin@mail.ru>
2025-12-05 12:01:45 +0400

    Refactor print_alphabet_x10 for better readability
    Corrected formatting and added a space for readability.

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,16 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet 10 times  in lowercase, followed by a new line.
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase.
  */
 
 void print_alphabet_x10(void)
 {
 	char letter;
-	int i=0;
+	int i = 0;
 
-	while (i<10) {
+	while (i < 10)
+	{
 	for (letter = 'a' ; letter <= 'z' ; letter++)
 		_putchar(letter);
 	i++;

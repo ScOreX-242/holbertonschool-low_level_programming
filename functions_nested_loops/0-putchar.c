2e5aef5dc7b414ad7bd5c285cf808727f60d9f4c
ScOreX-242 <israfilovamin@mail.ru>
2025-12-05 10:31:51 +0400

    Fix character literals in _putchar function
    Updated character literals to use single quotes for _putchar function calls.

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,26 +1,24 @@
-/*
- * File: 0-putchar.c
- * Auth: Amin Israfilov <12640@holbertonstudents.com>
- */
-
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Program that prints all possible combinations of single-digit numbers.
+ * main - Print a string
  *
- * Return: Always 0.
+ * My function: prints _putchar
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    _putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
-	
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }

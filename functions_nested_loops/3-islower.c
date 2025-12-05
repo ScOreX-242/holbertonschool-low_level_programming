c89f59ec17c45fe84c3fe06b58ed3a26f3dae4f1
ScOreX-242 <israfilovamin@mail.ru>
2025-12-05 12:37:58 +0400

    Implement _islower function for lowercase check
    Fix function to check for lowercase letters.

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,1 +1,20 @@
-heheheeh
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Amin Israfilov
+ */
+
+#include "main.h"
+
+/**
+ * int _islower(int c) - function that checks for lowercase character. 
+ */
+
+int _islower(int c)
+{
+char c;
+        if (c = 'a' && c <= 'z')
+            return(1);
+        else
+            return(0);
+	}
+}

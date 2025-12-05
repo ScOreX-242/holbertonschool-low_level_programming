b6a5e7b0b91200eb40b46ad16c41f61b87c4547c
Amin Israfilov <israfilovamin@mail.ru>
2025-12-05 07:37:46 +0000

    1 task

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,8 +7,14 @@
  * Return: Always 0.
  */
 
+void print_alphabet(void);
+
 int main(void)
 {
-	print_alphabet();
-	return (0);
+	char letter;
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+		_putchar(letter)
+
+	_putchar("\n");
+	return(0);
 }

77350c34e48c18a101c3360a104e9b5600be945b
ScOreX-242 <israfilovamin@mail.ru>
2025-12-07 00:40:45 +0400

    Create 1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+/*
+ * File: 1-isdigit.c
+ * Auth: Amin Israfilov
+ */
+
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+
+	else
+		return (0);
+}

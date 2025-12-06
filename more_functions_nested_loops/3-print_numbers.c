46c09dbc69a102c15cb253eafc7eccf9e3aeaf10
ScOreX-242 <israfilovamin@mail.ru>
2025-12-07 02:15:26 +0400

    Create 3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+/*
+ * File: 3-print_numbers.c
+ * Auth: Amin Israfilov
+ */
+
+#include "main.h"
+
+/**
+ * mul - Multiples two integers.
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ *
+ * Return: The result of the multiplication.
+ */
+void print_numbers(void)
+{
+	char digit = 48;
+  for(digit; digit>=48 && digit<=57; digit++)
+    _putchar(digit);
+  _putchar('\n');
+  return(0);
+}

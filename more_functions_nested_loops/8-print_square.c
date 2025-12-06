589b1bcf3644c4a4b9bfd77b2a5b05c7b744b432
ScOreX-242 <israfilovamin@mail.ru>
2025-12-07 03:26:55 +0400

    Update 8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,13 +1,34 @@
+/*
+ * File: 7-print_square.c
+ * Auth: Amin Israfilov
+ */
+
 #include "main.h"
+
+/**
+ * print_square - Prints a square followed by a new line.
+ * @size: The size length of the square.
+ *
+ * Description: Uses the character '#' to draw the square.
+ * If size is 0 or less, only prints a new line.
+ * Return: void.
+ */
 void print_square(int size)
 {
-    int i;
-    int lines;
-  
-    for(lines=0;lines<size;lines++)
-    {
-        for(i=0; i<size ; i++)
-            putchar('#');
-      putchar('\n');
-    }
+	int i;
+	int lines;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (lines = 0; lines < size; lines++)
+	{
+		for (i = 0; i < size; i++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 }

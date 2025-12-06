bb1540b71963ce2c8ac05628409c88c5d8ba7296
ScOreX-242 <israfilovamin@mail.ru>
2025-12-06 13:47:11 +0400

    Implement print_sign function with proper checks
    Fix function definition and add documentation.

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,1 +1,30 @@
-5 task
+/*
+ * File: 5-sign.c
+ * Auth: Amin Israfilov
+ */
+
+#include "main.h"
+
+/**
+ * print_sign - function that prints the sign of a number.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if n > 0, 0 if n = 0 , -1 and - if n < 0.
+ */
+
+int print_sign(int n)
+{
+	if (n>0)
+	{
+    _putchar('+');
+		return(1);
+	}
+	else if (n=0) {
+    _putchar('0');
+    return(0);
+  }
+  else if (n<0) {
+    _putchar('-');
+    return(-1);
+  }
+}

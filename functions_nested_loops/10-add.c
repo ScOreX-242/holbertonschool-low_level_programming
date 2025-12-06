2ace1ed859b59c9efc3c5a3c724120b713e081d1
ScOreX-242 <israfilovamin@mail.ru>
2025-12-06 17:32:16 +0400

    Refactor add function and improve documentation
    Updated function documentation and formatting.

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -6,12 +6,16 @@
 #include "main.h"
 
 /**
- * int add() - function that adds two integers and returns the result.
+ * add - Adds two integers.
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
+ *
+ * Return: The sum of the two integers.
  */
 int add(int a, int b)
 {
-  int sum;
-  
-  sum = a+b;
-  return(sum);
+	int sum;
+
+	sum = a + b;
+	return (sum);
 }

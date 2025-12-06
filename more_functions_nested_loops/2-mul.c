16fc14139278edb3b58838f62e97d7af994f9c70
ScOreX-242 <israfilovamin@mail.ru>
2025-12-07 02:03:48 +0400

    Create 2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-mul.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+
+/**
+ * _isdigit - Checks for a digit (0-9).
+ * @c: The number to be checked.
+ *
+ * Return: 1 if the number is a digit, 0 otherwise.
+ */
+int mul(int a, int b)
+{
+  mul = a * b;
+	return (mul);
+}

935795ec67497fea3cde1a7c9156711286583f97
ScOreX-242 <israfilovamin@mail.ru>
2025-12-06 17:29:30 +0400

    Create 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/*
+ * File: 10-add.c
+ * Auth: Amin Israfilov
+ */
+
+#include "main.h"
+
+/**
+ * int add() - function that adds two integers and returns the result.
+ */
+int add(int a, int b)
+{
+  int sum;
+  
+  sum = a+b;
+  return(sum);
+}

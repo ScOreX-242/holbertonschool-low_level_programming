b1b72169bb60f57f8072eb7401977be26d097083
ScOreX-242 <israfilovamin@mail.ru>
2025-12-06 13:35:09 +0400

    Implement _isalpha function
    Add function to check for alphabetic characters.

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,1 +1,25 @@
-gg
+/*
+ * File: 4-isalpha.c
+ * Auth: Amin Israfilov
+ */
+
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for alphabetic character.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

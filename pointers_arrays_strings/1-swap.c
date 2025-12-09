e8e20a2993066e74a05b221b86525ad294b80b5d
ScOreX-242 <israfilovamin@mail.ru>
2025-12-09 12:46:56 +0400

    Create 1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+/*
+ * File: 1-swap.c
+ * Auth: Amin Israfilov
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - function that swaps the values of two integers.
+ * @a: The pointer to an int.
+ * @b: The pointer to an int.
+ */
+
+void swap_int(int *a, int *b)
+{
+  int p;
+  int c;
+  p = *a;
+  c = *c;
+  b = *p;
+  a = *c;
+}

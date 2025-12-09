91c1aa3154da41d67958136f484f2b28e083d056
ScOreX-242 <israfilovamin@mail.ru>
2025-12-09 13:05:16 +0400

    Create 2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Amin Israfilov
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*str++)
+		length++;
+
+	return (length);
+}

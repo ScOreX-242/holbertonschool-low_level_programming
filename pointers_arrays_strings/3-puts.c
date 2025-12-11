bdb192b890812c1b90fe945a09d3a87414cd8447
ScOreX-242 <israfilovamin@mail.ru>
2025-12-11 11:44:09 +0400

    Refactor _puts function for improved readability

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -6,18 +6,17 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @s: The string to get the length of.
- *
- * Return: The length of @str.
+ * _puts - Prints a string followed by a new line to stdout
+ * @str: The string to print
  */
 void _puts(char *str)
 {
-    int i = 0;
-    while(str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }

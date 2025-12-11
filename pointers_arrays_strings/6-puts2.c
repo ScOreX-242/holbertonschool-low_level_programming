3c94996e076e576c38534c006381d72d077ecc1e
ScOreX-242 <israfilovamin@mail.ru>
2025-12-11 23:59:53 +0400

    Update 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
- * puts2 - Function that prints every other character of a string, starting with the first character
+ * puts2 - Function that prints every other character of a string.
  * @str: The string to modify
  */
 void puts2(char *str)

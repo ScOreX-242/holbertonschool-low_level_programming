d4f7b1c636b1e270db28f7e17cdea9ebbc7e70c9
ScOreX-242 <israfilovamin@mail.ru>
2025-12-11 10:27:21 +0400

    Create 3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Amin Israfilov
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+void _puts(char *str)
+{
+    puts(str);
+    return 0;
+}

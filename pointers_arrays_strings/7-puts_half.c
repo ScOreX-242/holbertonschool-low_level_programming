7fee65bc97022a32f739400cc7c79cc5b0a81ab2
ScOreX-242 <israfilovamin@mail.ru>
2025-12-12 11:44:28 +0400

    Update 7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,25 +11,33 @@
  */
 void puts_half(char *str)
 {
-  int len = 0;
+void puts_half(char *str)
+{
+	int len = 0;
 	int i = 0;
-  int half_len;
+	int half_len;
 
 	while (str[len] != '\0')
 	{
 		len++;
 	}
-  if (len % 2 == 0)
-  {
-    half_len = len / 2 ;
-    for(i += half_len; i < len; i += 1)
-      _putchar(str[i]);
-  }
-  else
-{
-    half_len = ( len + 1 ) / 2;
-    for(i += half_len; i < len; i += 1)
-      _putchar(str[i]);
-}
+
+	if (len % 2 == 0)
+	{
+		half_len = len / 2;
+		for (i = half_len; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		half_len = (len + 1) / 2;
+		for (i = half_len; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
 	_putchar('\n');
+}
 }

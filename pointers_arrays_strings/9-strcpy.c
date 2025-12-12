7c344b1f99438104e3ad11349a1506565458ad74
ScOreX-242 <israfilovamin@mail.ru>
2025-12-13 00:52:11 +0400

    Update 9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -15,14 +15,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int count = 0;
+	int i = 0;
 
-	while (count >= 0)
+	while (src[i])
 	{
-		*(dest + count) = *(src + count);
-		if (*(src + count) == '\0')
-			break;
-		count++;
+		dest[i] = src[i];
+		i++;
 	}
+	*(dest + i) = '\0';
 	return (dest);
 }

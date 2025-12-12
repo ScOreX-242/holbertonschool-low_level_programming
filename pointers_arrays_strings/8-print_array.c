b59dfe5b856144a778f35acd8490d6fa069756e2
ScOreX-242 <israfilovamin@mail.ru>
2025-12-12 12:27:56 +0400

    Update 8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -17,8 +17,8 @@ void print_array(int *a, int n)
     for(i=0; i<n-1; i++)
       {
         printf("%d, ", a[i]);
-        if(i==n-1)
-        printf("%d", a[n]);
+        if(i=n-1)
+        printf("%d", a[n-1]);
       }
 	_putchar('\n');
 }

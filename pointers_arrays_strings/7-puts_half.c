5e3b31d492ff42432c53e2b15ed0601b2d354be3
ScOreX-242 <israfilovamin@mail.ru>
2025-12-12 11:42:13 +0400

    Update 7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,24 +6,22 @@
 #include "main.h"
 
 /**
- * puts2 - Function that prints half of a string
+ * puts_half - Function that prints half of a string
  * @str: The string to modify
  */
 void puts_half(char *str)
 {
   int len = 0;
 	int i = 0;
-  int norm_len;
   int half_len;
 
 	while (str[len] != '\0')
 	{
 		len++;
 	}
-  norm_len = len - 1;
-  if (norm_len % 2 == 0)
+  if (len % 2 == 0)
   {
-    half_len = norm_len / 2 ;
+    half_len = len / 2 ;
     for(i += half_len; i < len; i += 1)
       _putchar(str[i]);
   }

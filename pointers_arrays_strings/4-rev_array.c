870b6c59dfb462645d84937c2784ab56b8f7ba63
ScOreX-242 <israfilovamin@mail.ru>
2025-12-14 23:25:35 +0400

    Implement reverse_array function to reverse an array
    This function swaps elements to reverse the order of an integer array.

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+/**
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: The array of integers to be reversed.
+ * @n: The number of elements in the array.
+ *
+ * Description:
+ * The function reverses the order of elements in the array 'a'
+ * by swapping the first element with the last, the second with
+ * the second-to-last, and so on.
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}

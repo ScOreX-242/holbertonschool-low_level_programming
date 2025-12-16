264d3b8def9f13c30b31f0b149c53cb631fd7772
ScOreX-242 <israfilovamin@mail.ru>
2025-12-16 11:01:18 +0400

    Create 2-strlen_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints function that returns the length of a string.
+ * @s: the string to print
+ */
+int _strlen_recursion(char *s)
+{
+  int i = 0;
+	if (*s == '\0')
+	{
+		return;
+	}
+  i++;
+	_print_rev_recursion(s + 1);
+  printf("%d", i);
+}

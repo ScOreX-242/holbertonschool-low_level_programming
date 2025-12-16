bb8836e5148301738f8046dde63ef093fa88f00e
ScOreX-242 <israfilovamin@mail.ru>
2025-12-16 11:22:28 +0400

    Create 3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number.
+ * @s: the string to measure
+ *
+ * Return: the length of the string
+ */
+int factorial(int n)
+{
+  if (n < 0)
+    return -1;
+  factorial(n+1);
+  return(n * factorial(n+1));
+}

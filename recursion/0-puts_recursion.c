2f369163ef5a1337ad9ab95099b40bae817fda1a
ScOreX-242 <israfilovamin@mail.ru>
2025-12-16 09:57:04 +0400

    Create 0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+void _puts_recursion(char *s)
+{
+    int i = 0;
+    int len = 0;
+    int ii;
+    while(s[i] != '\0')
+    {
+        len++;
+        i++;
+    }
+    for(ii = 0 ; ii < len ; ii++)
+        putchar(s[ii]);
+    return 0;
+}

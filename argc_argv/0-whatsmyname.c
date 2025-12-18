2c0859dff3fff129612d4725e255e264a316bc65
ScOreX-242 <israfilovamin@mail.ru>
2025-12-18 14:02:04 +0400

    Change program to print its name instead of argument count

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the number of arguments passed to the program.
+ * main - Prints the name of the program.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
@@ -9,9 +9,9 @@
  */
 int main(int argc, char *argv[])
 {
-	(void)argv;
+	(void)argc;
 
-	printf("%d\n", argc);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }

96042e2d9f280a46a1dd0cb794c00c333de84098
ScOreX-242 <israfilovamin@mail.ru>
2025-12-18 14:02:22 +0400

    Add program to print number of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed to the program.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}

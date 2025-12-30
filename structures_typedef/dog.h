cdd568a3e15e71e7cecf867d405eddb70e02d832
ScOreX-242 <israfilovamin@mail.ru>
2025-12-31 00:01:31 +0400

    Document and redefine dog struct in dog.h
    Added a detailed comment block for the dog struct and redefined it in main.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,17 @@
-struct dog {
-char *name;
-float age;
-char *owner;
+/**
+ * struct dog - a struct that stores basic information about a dog
+ * @name: First member, pointer to a string for the dog's name
+ * @age: Second member, float representing the dog's age
+ * @owner: Third member, pointer to a string for the owner's name
+ *
+ * Description: This structure is used to group the identity, age,
+ * and ownership details of a canine.
+ */
+int main(void){
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 }

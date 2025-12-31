a38a3c91b458e0b65a231cbbd9e566322054e8e8
ScOreX-242 <israfilovamin@mail.ru>
2025-12-31 16:21:54 +0400

    Update and rename funpointers.h to function_pointers.h

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#IFNDEF FUNCTION_POINTERS_H
+#DEFINE FUNCTION_POINTERS_H
+
+void print_name(char *name, void (*f)(char *));
+
+#ENDIF /* FUNCTION_POINTERS_H */
--- function_pointers/funpointers.h
+++
@@ -1,6 +0,0 @@
-#IFNDEF FUNPOINTERS_H
-#DEFINE FUNPOINTERS_H
-
-void print_name(char *name, void (*f)(char *));
-
-#ENDIF /* FUNPOINTERS_H */

5b3f8f87956ea740838a49ebe984f79ae41b20b4
ScOreX-242 <israfilovamin@mail.ru>
2025-12-31 16:21:02 +0400

    Create 0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "funpointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: pointer to a function that takes a char * and returns void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name != NULL && f != NULL)
+    {
+        f(name);
+    }
+}
